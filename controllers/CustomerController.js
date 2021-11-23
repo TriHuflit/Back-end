388fa6e5d871d3688deab35c2e7ff14b4aa5652e
unknown <trilxag1001@gmail.com>
2021-11-23 10:50:10 +0700

    fix db

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -75,7 +75,7 @@ class CustomerController {
             })
         } catch (error) {
             console.log(error);
-            res.status(500).json({ success: false, message: error })
+            res.status(500).json({ success: false, message: error,customer })
         }
     }
 
