c20ce8779fc5cb96d546fc0065258bdce154da0c
unknown <trilxag1001@gmail.com>
2021-12-13 15:25:52 +0700

    news

--- controllers/NewsController.js
+++ controllers/NewsController.js
@@ -65,13 +65,46 @@ class NewsController {
         .status(404)
         .json({ success: false, message: "News Not Found" });
     }
+    return res.status(200).json({ success: true, news });
+  }
+  //[POST] api/news/update/:slug
+  async update(req, res) {
+    const { author, title, content } = req.body;
+
+    const news = await News.findOne({ slug: req.params.slug });
+    if (!news) {
+      return res
+        .status(404)
+        .json({ success: false, message: "News Not Found" });
+    }
     try {
-      const detailNews = await DetailNews.find({ idNews: news._id });
+      await cloudinary.uploader.destroy(news.image.cloud_id);
+      const image = await cloudinary.uploader.upload(req.body.image);
+      let updateNews = {
+        author,
+        title,
+        content,
+        image: {
+          url: image.secure_url,
+          cloud_id: image.public_id,
+        },
+      };
+      const Newsupdate = await News.findByIdAndUpdate(
+        { _id: news._id },
+        updateNews,
+        { new: true }
+      );
+      if (!Newsupdate) {
+        return res
+          .status(400)
+          .json({ success: false, message: "Update News Failed" });
+      }
+      return res
+        .status(200)
+        .json({ success: true, message: "Update News Successfully" });
     } catch (err) {
       return res.status(500).json(err);
     }
   }
-  //[POST] api/news/update/:slug
-  async update(req, res) {}
 }
 module.exports = new NewsController();
