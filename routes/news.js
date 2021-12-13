12cc23ac43176a8d32beec1818f67363569baee3
unknown <trilxag1001@gmail.com>
2021-12-13 23:40:00 +0700

    fix

--- routes/news.js
+++ routes/news.js
@@ -2,14 +2,14 @@ const NewsController = require("../controllers/NewsController");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", NewsController.index);
-
-router.get("/:slug", NewsController.detail);
+router.put("/update/:slug", NewsController.update);
 
 router.post("/store", NewsController.store);
 
 router.delete("/delete/:slug", NewsController.delete);
 
-router.put("/update/:slug", NewsController.update);
+router.get("/:slug", NewsController.detail);
+
+router.get("/", NewsController.index);
 
 module.exports = router;
